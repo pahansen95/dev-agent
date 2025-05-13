55cdba0251f4944255913fbce0c141a05a5980de
Peter Hansen <mail@peterhansen.io>
2025-05-13 12:48:05 +0000

    Cleanup project

--- vendor/WebProtege/setenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Set proper system properties for Tomcat and WebProtege
-export CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Dwebprotege.config.directory=/etc/webprotege -Ddata.directory=/srv/webprotege"
